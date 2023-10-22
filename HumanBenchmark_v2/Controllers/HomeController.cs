e82f4792d998ffe08603097dbaf39ef280027fad
Archox4 <archoxxr@gmail.com>
2023-10-22 18:23:23 +0200

    Dodaj pliki projektów.

---
+++ HumanBenchmark_v2/Controllers/HomeController.cs
@@ -0,0 +1,109 @@
+﻿using HumanBenchmark_v2.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using Firebase.Auth;
+using HumanBenchmark_v2.Models;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace HumanBenchmark_v2.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        FirebaseAuthProvider auth;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+            auth = new FirebaseAuthProvider(new FirebaseConfig("AIzaSyA-eoPydpcHMoMQuuwJtGaPSp_ySJhgPew"));
+            
+        }
+
+        public IActionResult Index()
+        {
+            ViewBag.AuthProvider = auth;
+            var token = HttpContext.Session.GetString("_UserToken");
+            if (token != null)
+            {
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("SignIn");
+            }
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        public IActionResult SignIn()
+        {
+            return View();
+        }
+        public IActionResult ReactionTime()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(UserModel userModel)
+        {
+            //create the user
+            await auth.CreateUserWithEmailAndPasswordAsync(userModel.Email, userModel.Password);
+            //log in the new user
+            var fbAuthLink = await auth
+                            .SignInWithEmailAndPasswordAsync(userModel.Email, userModel.Password);
+            string token = fbAuthLink.FirebaseToken;
+            //saving the token in a session variable
+            if (token != null)
+            {
+                HttpContext.Session.SetString("_UserToken", token);
+
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View();
+            }
+        }
+        [HttpPost]
+        public async Task<IActionResult> SignIn(UserModel userModel)
+        {
+            //log in the user
+            var fbAuthLink = await auth
+                            .SignInWithEmailAndPasswordAsync(userModel.Email, userModel.Password);
+            string token = fbAuthLink.FirebaseToken;
+            //saving the token in a session variable
+            if (token != null)
+            {
+                HttpContext.Session.SetString("_UserToken", token);
+
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View();
+            }
+        }
+        public IActionResult LogOut()
+        {
+            HttpContext.Session.Remove("_UserToken");
+            return RedirectToAction("SignIn");
+        }
+    }
+}
